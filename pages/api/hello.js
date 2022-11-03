605a83e3ece06bd622b1e2c298c834be932d988d
cis <chals001@naver.com>
2022-11-03 21:10:08 +0900

    prisma 추가
    prisma 추가 후 db insert 테스트

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+
+  if(req.method === 'GET'){
+    const userProfile = await prisma.userProfile.findMany();
+    res.status(200).json({ userName: userProfile })
+
+  } else if(req.method === 'POST'){
+    const {body: data} = req;
+    
+    const newUser = await prisma.userProfile.create({data});
+
+    res.status(200).json({createStatus : 'ok'});
+    return res.status(201).send(newUser);
+  } else {
+    
+  }
 }
